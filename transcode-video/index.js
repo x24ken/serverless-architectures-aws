4394e91681df1e817bb13f5d4caff01bfd036feb
x24ken <x24ken@gmail.com>
2018-05-25 09:05:04 +0000

    3.2終わり

--- transcode-video/index.js
+++ transcode-video/index.js
@@ -2,43 +2,45 @@
 
 var AWS = require('aws-sdk');
 
-var electricTranscoder = new AWS.ElasticTranscoder({
+var elasticTranscoder = new AWS.ElasticTranscoder({
     region: 'us-east-1'
 });
 
 exports.handler = function(event, context, callback){
     
+    console.log("Welcome!");
+    
     var key = event.Records[0].s3.object.key;
     
-    var sourceKey = decodeURIComponent(key.replace(/\+/g, ""));
+    var sourceKey = decodeURIComponent(key.replace(/\+/g, ''));
     
-    var outputKey = sourceKey.split[0];
+    var outputKey = sourceKey.split('.')[0];
     
     console.log('key:', key, sourceKey, outputKey);
     
     var params = {
-        PipelineId: '1527151061750-mppihh',
-        OutputKeyPrefix: outputKey + '/',
-        Input: {
-            Key: sourceKey
-        },
-        Outputs: [
-        {
-            Key: outputKey + '-1080p' + '.mp4',
-            PresetId: '1351620000001-000001'
-        },
-        {
-            Key: outputKey + '-720p' + '.mp4',
-            PresetId: '1351620000001-000010'
-        },
-        {
-            Key: outputKey + '-web-720p' + 'mp4',
-            PresetId: '1351620000001-100070'
-        }
+            PipelineId: '1527151061750-mppihh',
+            OutputKeyPrefix: outputKey + '/',
+            Input: {
+                Key: sourceKey
+            },
+            Outputs: [
+            {
+                Key: outputKey + '-1080p' + '.mp4',
+                PresetId: '1351620000001-000001'
+            },
+            {
+                Key: outputKey + '-720p' + '.mp4',
+                PresetId: '1351620000001-000010'
+            },
+            {
+                Key: outputKey + '-web-720p' + '.mp4',
+                PresetId: '1351620000001-100070'
+            }
     ]};
-    electricTranscoder.createJob(params, function(err, data){
-        if (err){
-            callback(err);
+    elasticTranscoder.createJob(params, function(error, data){
+        if (error){
+            callback(error);
         }
     });
 };
