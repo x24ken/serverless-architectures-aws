dc39be512ee21c8c99eacf2930d36d3003b27d18
x24ken <x24ken@gmail.com>
2018-05-31 14:39:36 +0000

    ドット含む拡張子前のファイル取り出しエラー解決！

--- transcode-video/index.js
+++ transcode-video/index.js
@@ -7,19 +7,19 @@ var elasticTranscoder = new AWS.ElasticTranscoder({
     region: 'us-east-1'
 });
 
+
 exports.handler = function(event, context, callback){
     
     console.log("Welcome!");
     
     var key = event.Records[0].s3.object.key;
-    
     var sourceKey = decodeURIComponent(key.replace(/\+/g, ' '));
     
     
-    //課題１ どっと含む拡張子前のファイル名を取り出す
+    //課題１ ドット含む拡張子前のファイル名を取り出す
     var outputKey = path.basename(sourceKey, path.extname(sourceKey));
     
-    console.log('key:', key, sourceKey, outputKey);
+    console.log('key:', key, 'sourceKey:', sourceKey, 'outputKey', outputKey);
     
     //課題２ 拡張子のチェック
     
